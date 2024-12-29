901034c05944ed2d126d940fe0544cdcfd529409
Anton Pavlovicius <anton.pavlovicius@gmail.com>
2024-12-29 17:29:24 +0200

    dev branch commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
         <p>Vilnius Krruuu</p>
         <p>Vilnius Krruuu</p>
         <p>dev branch commit 1</p>
+        <p>dev branch commit 22</p>
         <a
           className="App-link"
           href="https://reactjs.org"
